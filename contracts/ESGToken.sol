28c787ef1b27e29ff64a681e4abedeca984057f5
willianeisidoro <willianeisidoro@gmail.com>
2026-04-23 23:54:14 -0300

    Created ESGToken-ERC20

---
+++ contracts/ESGToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.6.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract ESGToken is ERC20, ERC20Permit {
+    constructor(address recipient)
+        ERC20("ESGToken", "ESG")
+        ERC20Permit("ESGToken")
+    {
+        _mint(recipient, 1000000 * 10 ** decimals());
+    }
+}
