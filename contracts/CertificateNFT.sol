fd2b7eb1b5b935408a084a5b5e8d2bbf3a0f45fd
willianeisidoro <willianeisidoro@gmail.com>
2026-04-25 00:00:51 -0300

    ERC 721 done

--- contracts/CertificateNFT.sol
+++ contracts/CertificateNFT.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CertificateNFT is ERC721, Ownable {
+contract CertificateNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
-    mapping(uint256 => string) private _tokenURIs;
 
-    constructor() ERC721("ESG Certificate", "ESGCERT") {}
+    constructor() 
+        ERC721("ESG Certificate", "ESGCERT") 
+        Ownable(msg.sender) 
+    {}
 
     function mintCertificate(address to, string memory tokenURI)
         public
@@ -22,20 +24,5 @@ contract CertificateNFT is ERC721, Ownable {
         _setTokenURI(newTokenId, tokenURI);
 
         return newTokenId;
-    }
-
-    function _setTokenURI(uint256 tokenId, string memory tokenURI) internal {
-        require(_exists(tokenId), "Token inexistente");
-        _tokenURIs[tokenId] = tokenURI;
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
-        require(_exists(tokenId), "Token inexistente");
-        return _tokenURIs[tokenId];
     }
 }
